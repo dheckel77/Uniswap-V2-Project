daab547809ed150936d53c68cb8d5ed78cba9340
dheckel77 <dheckel77@gmail.com>
2024-08-26 16:10:13 -0400

    changed ethers version

--- client/GetPrices.js
+++ client/GetPrices.js
@@ -3,7 +3,7 @@ const { addressFactory, addressRouter, addressFrom, addressTo } = require("./Add
 const { erc20ABI, factoryABI, routerABI, pairABI } = require("./AbiList");
 
 // Standard Provider
-const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/");
+const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/gp5LSHc4qKn3iMUYagaY76nSHVN9ZHWv");
 
 // Connect to Factory
 const contractFactory = new ethers.Contract(addressFactory, factoryABI, provider);
@@ -34,7 +34,7 @@ const getPrices = async (amountInHuman) => {
    return amountsOutHuman;
 };
 
-const amountInHuman = "500";
+const amountInHuman = "1";
 getPrices(amountInHuman);
 
 // console.log(contractRouter);
